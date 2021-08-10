833e6e11664221c9980e99a3d363df2bd9cd0e2c
Jean <pesajet@gmail.com>
2021-08-10 17:06:33 +0300

    uga

---
+++ app/src/main/java/com/example/ugcares/ClientSignInForm.kt
@@ -0,0 +1,11 @@
+package com.example.ugcares
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClientSignInForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_sign_in_form)
+    }
+}
