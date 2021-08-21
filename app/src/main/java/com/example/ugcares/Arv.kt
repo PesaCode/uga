3171362f57977860bca6cd178e8e6240ceef5b2d
Jean <pesajet@gmail.com>
2021-08-21 14:57:40 +0300

    Second

---
+++ app/src/main/java/com/example/ugcares/Arv.kt
@@ -0,0 +1,11 @@
+package com.example.ugcares
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Arv : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arv)
+    }
+}
--- app/src/main/java/com/example/ugcares/MainActivity.kt
+++ app/src/main/java/com/example/ugcares/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ugcares
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+Handler().postDelayed({
+    startActivity(Intent(this,ClientSignInForm::class.java))
+    finish()
+},2500)
     }
+//    val typeface: Typeface =Typeface.createFromAsset(assets,"Montserrat-Bold.ttf")
+//    tv_app_name.typeface=typeface
 }
